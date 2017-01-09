7dbe7744449afec8775883ff14fc0a8d00455138
BenSchlereth <schlerethbenedikt@gmail.com>
2017-01-09 18:38:58 +0100

    Writeregister Debuged without a RasPi

--- scoreboard/html/learn-jquery.js
+++ scoreboard/html/learn-jquery.js
@@ -1,5 +1,7 @@
 // score input and output
 
+
+
 //init
 var scoreHome = 0;
 var scoreGuest = 0;
@@ -9,12 +11,79 @@ $('#home').text(scoreHome);
 $('#guest').text(scoreGuest);
 
 
+function writeregister(n)
+{
+//console.log(n);
+var count = 7;
+var byte;
+byte = Math.floor(n%10);
+//console.log(byte);
+console.log(typeof(byte));
+	do
+        {
+                if(count===0)
+                {
+			write1();
+                }
+                else if(count===1 && (byte===1 || byte===4))
+                {
+                        write1();
+                }
+                else if(count===2 && (byte===5 || byte===6)) 
+                {       
+			write1();
+                }
+                else if(count===3 && byte===2)
+                {       
+			write1();
+                }
+                else if(count===4 && (byte===1 || byte===4 || byte===7)) 
+                {
+                        write1();
+                }
+                 else if(count===5 && (byte===1 || n===3 || n===4 || n===5 || n===7 || n===9))
+                {
+			write1();
+                }
+                else if(count===6 && (n===1 || n===2 || n===3 || n===7))
+                {
+			write1();
+                }
+                else if(count===7 && (n===0 || n===1 || n===7))
+                {
+			write1();
+                }
+                else
+                {
+			write0();
+                }
+      		count--;                                        //decrement count
+        } while (count>=0);                               //exit when Bit0 is set
+}
+
+function write1()
+{
+var bit=1;
+console.log(bit);
+}
+
+function write0()
+{
+var bit=0;
+console.log(bit);
+}
+
 //score for Home team
 document.querySelector('box1').onclick = function() 
 {
 	var score = prompt('How many points where scored?');
 	scoreHome = +scoreHome + +score;			//For adding numbers and not strings the (+) before the variable is needed
 	$('#home').text(scoreHome);
+	writeregister(scoreHome);
+	if(scoreHome>9) writeregister(scoreHome/10);
+	else writeregister(0);	 
+		
+	
 }
 
 
@@ -52,7 +121,7 @@ setTimeout(Decrement,1000);
 
 document.querySelector('h2').onclick = function() 
 {
-	if (bool != 1) bool = 1;
+	if (bool !== 1) bool = 1;
 	else bool = 0; 
 }
 
@@ -60,11 +129,11 @@ function Decrement()
 {
 document.querySelector('h2').onclick = function() 
 {
-	if (bool != 1) bool = 1;
+	if (bool !== 1) bool = 1;
 	else bool = 0; 
 }
 
-	if (bool != 0)
+	if (bool !== 0)
 	{        
 		currentMinutes = Math.floor(secs / 60);
 		currentSeconds = secs % 60;
